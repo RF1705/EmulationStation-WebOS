0abf10581f14f6323ec723818fc675910fe58417
Ronald Fiering <rfiering@googlemail.com>
2026-08-07 17:06:03 +0200

    Apply verified Magic Remote mappings

--- scripts/build-webos.sh
+++ scripts/build-webos.sh
@@ -19,6 +19,7 @@ done
 [[ -f "$vcpkg_root/scripts/buildsystems/vcpkg.cmake" ]] || { echo "vcpkg toolchain not found below $vcpkg_root" >&2; exit 1; }
 
 python3 "$repo_root/scripts/patch-retropie-webos.py" "$source_dir"
+python3 "$repo_root/scripts/patch-retropie-webos-remote.py" "$source_dir"
 
 sdl_include="$(find "$SDL2_BUNDLE_DIR" -path '*/include/SDL2/SDL.h' -print -quit)"
 sdl_library="$(find "$SDL2_BUNDLE_DIR" -name 'libSDL2-2.0.so.0' -print -quit)"
